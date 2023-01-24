e734682370e91436a603658f5bfb9d1ea3f0774a
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-01-24 21:39:32 +0100

    made the login and sign-in routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const { check, validationResult } = require("express-validator")
 const { users } = require("../db")
 const bcrypt = require("bcrypt")
+const JWT = require("jsonwebtoken")
 
 router.post("/signup",[
   check("email","Please provide a valid email")
@@ -27,7 +28,7 @@ router.post("/signup",[
   })
   
   if(user){
-    res.status(400).json({
+    return res.status(400).json({
       "errors": [
         {
           "msg": "Email already exists"
@@ -42,9 +43,55 @@ router.post("/signup",[
     password: hashedPassword
   })
 
-  res.send("Validation Past")
+  const token = await JWT.sign({
+    email
+  }, "ghfsjjhfhhdfjhdjhjdshuewiukdkk62874hffnm", {
+    expiresIn: 360000
+  })
+
+  res.json({
+    token
+  })
 })
 
+router.post("/login", async (req,res) => {
+  const { email, password} = req.body
+  let user = users.find((user) => {
+    return user.email === email
+  })
+
+  if(!user){
+    return res.status(400).json({
+      "errors": [
+        {
+          "msg": "Invalid Credentials"
+        }
+      ]
+    })
+  }
+
+  let isMatch = await bcrypt.compare(password,user.password)
+
+  if(!isMatch){
+    return res.status(400).json({
+      "errors": [
+        {
+          "msg": "Invalid Credentials"
+        }
+      ]
+    })
+  }
+
+  const token = await JWT.sign({
+    email
+  }, "ghfsjjhfhhdfjhdjhjdshuewiukdkk62874hffnm", {
+    expiresIn: 360000
+  })
+
+  res.json({
+    token
+  })
+})
 router.get("/all", (req,res) => {
   return res.json(users)
 })
